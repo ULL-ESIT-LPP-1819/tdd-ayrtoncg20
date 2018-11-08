f88f0317b4e0d006318dc93dc6d48042a2bcaf83
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-08 07:42:57 +0000

    Creando la prueba para el nombre de la etiqueta

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,3 +1,11 @@
 class PruebasNutricion
-	
+
+	#def initialize(nombre)
+	#	@nombre = nombre
+	#end
+
+	#def nombre_etiqueta
+	#	@nombre
+	#end
+		
 end
--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe PruebasNutricion do
-	before :each do
-	
+	before :all do
+		
+		@cereales_chocolate = PruebasNutricion.new("Cereales de chocolate")
+
+
+	end
+
+	describe "expectativas" do
+
+		it "Existe un nombre para la etiqueta" do
+			expect(@cereales_chocolate.nombre_etiqueta).to eq("Cereales de chocolate");
+		end
 	end
 end
-	
+
