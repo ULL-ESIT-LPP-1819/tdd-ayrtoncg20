bd6be2d2436e1ecf1b03999c9dd8748f0df28ba6
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-08 10:20:59 +0000

    Creando el codigo para la prueba de los azucares

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,7 +1,7 @@
 class PruebasNutricion
 
-	def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono)
-		@nombre, @grasas, @grasas_saturadas, @hidratos_carbono = nombre, grasas, grasas_saturadas, hidratos_carbono
+	def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares)
+		@nombre, @grasas, @grasas_saturadas, @hidratos_carbono, @azucares = nombre, grasas, grasas_saturadas, hidratos_carbono, azucares
 	end
 
 	def nombre_etiqueta
@@ -18,5 +18,9 @@ class PruebasNutricion
 		
 	def hidratos_carbono
 		@hidratos_carbono
+	end
+
+	def azucares
+		@azucares
 	end
 end
