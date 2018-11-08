0ced06ae98957857d8327a55efe7882110d83552
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-08 10:18:03 +0000

    Creando la prueba para los azucares

--- spec/gema_spec.rb
+++ spec/gema_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe PruebasNutricion do
 	before :all do
 		
-		@cereales_chocolate = PruebasNutricion.new("Cereales de chocolate", 0.8, 0.2, 82.0)
+		@cereales_chocolate = PruebasNutricion.new("Cereales de chocolate", 0.8, 0.2, 82.0, 7.0)
 
 	end
 
@@ -24,6 +24,10 @@ RSpec.describe PruebasNutricion do
 
 		it "Existe una cantidad de hidratos de carbono" do
 			expect(@cereales_chocolate.hidratos_carbono).to eq(82.0)
+		end
+
+		it "Existe una cantidad de azúcares para el alimento" do
+			expect(@cereales_chocolate.azucares).to eq(7.0)
 		end
 
 	end
