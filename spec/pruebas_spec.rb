51bfcb9e5911e5e749d702aab62e04f6cf750286
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-20 20:13:28 +0000

    Creando la prueba para la edad del individuo

--- spec/pruebas_spec.rb
+++ spec/pruebas_spec.rb
@@ -12,5 +12,14 @@ RSpec.describe Individuo do
 		it "Existe un peso para el individuo" do 
 			expect(@individuo1.peso).to eq(65.0)
 		end
+
+		it "Existe una altura para el individuo" do
+			expect(@individuo1.talla).to eq(1.60)
+		end
+		
+		it "Existe una edad para el individuo" do
+			expect(@individuo1.edad).to eq(23.0)
+		end
+
 	end
 end
