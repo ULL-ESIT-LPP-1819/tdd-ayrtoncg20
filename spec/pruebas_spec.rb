db595e3493c9fff7b54fcd8b2986dc4f9ce5fc7b
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-20 23:58:32 +0000

    Creando las pruebas para tipo clase y jerarquia de objetos

--- spec/pruebas_spec.rb
+++ spec/pruebas_spec.rb
@@ -3,20 +3,41 @@ require 'spec_helper'
 RSpec.describe Individuo do
 	before :each do	
 
-		@Ayrton = Individuo.new(1, "Ayrton", "Crespo", "20-01-1995", "Estudiante", "No", 1)
+		@ayrton = Individuo.new(1, "Ayrton", "Crespo", "20-01-1995", "Estudiante", "No", 1)
 
 	end
 
 	describe "expectativas" do
 		
 		it "Existe un individuo formateado" do
-			expect(@Ayrton.to_s).to eq("[Numero De Historia=#{@Ayrton.numero_historia}, Nombre=#{@Ayrton.nombre}, Apellido=#{@Ayrton.apellido}, Fecha De Nacimiento=#{@Ayrton.fecha_nacimiento}, Ocupacion=#{@Ayrton.ocupacion}, Fumador=#{@Ayrton.fumador}, Sexo=#{@Ayrton.sexo}]:")
+			expect(@ayrton.to_s).to eq("[Numero De Historia=#{@ayrton.numero_historia}, Nombre=#{@ayrton.nombre}, Apellido=#{@ayrton.apellido}, Fecha De Nacimiento=#{@ayrton.fecha_nacimiento}, Ocupacion=#{@ayrton.ocupacion}, Fumador=#{@ayrton.fumador}, Sexo=#{@ayrton.sexo}]:")
 		end	
-	end
 
-	describe "Pacientes" do
-	
-		@Sara = Paciente.new(1, "Ayrton", "Crespo", "20-01-1995", "Estudiante", "No", 1, 83.0, 1.78, [60,65], [80,87], [2.5,2.6,2.7], [2.8,2.9,3.0], [3.1,3.2,3.3], [3.4,3.5,3.6], [20.0,21.2])
-	
-	end
+	 	it "Existe un método para comprobar el tipo de objeto" do
+      			expect(@ayrton.instance_of? Individuo).to eq(true)
+    		end
+
+   		it "Existe un método para comprobar la clase de un objeto" do
+      			expect(@ayrton.class).to eq(Individuo)
+      			expect(@ayrton.is_a? Individuo).to eq(true)
+    		end
+	   	
+		it "Existe un método para comprobar la jerarquía de un objeto" do
+      			expect(@ayrton.kind_of? Individuo).to eq(true)
+      			expect(@ayrton.kind_of? BasicObject).to eq(true)
+      			expect(@ayrton.kind_of? Object).to eq(true)
+    		end
+	end 
+	#describe Paciente do               
+	#	before :each do
+	#		@ayrton = Paciente.new(1, "Ayrton", "Crespo", "20-01-1995", "Estudiante", "No", 1, 83.0, 1.78, [60,65], [80,87], [2.5,2.6,2.7], [2.8,2.9,3.0], [3.1,3.2,3.3], [3.4,3.5,3.6], [20.0,20.8])
+	#	end
+	#
+
+	#describe "expectativas" do
+
+	#	 it "Existe un bicipital para el individuo" do
+         #       	 expect(@ayrton.bicipital).to eq((2.5+2.6+2.7)/3)
+	#	 end
+	#end
 end
