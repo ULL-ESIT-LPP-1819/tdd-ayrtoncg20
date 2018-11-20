211c90abbc24b5cfe65cc47dfc84ec1743d7d6c5
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-20 22:23:27 +0000

    Creando la prueba to_s para el individuo

--- spec/pruebas_spec.rb
+++ spec/pruebas_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 RSpec.describe Individuo do
 	before :each do	
 
-
 		@Ayrton = Individuo.new(1, "Ayrton", "Crespo", "20-01-1995", "Estudiante", "No", 1)
 
+	end
 
+	describe "expectativas" do
+		
+		it "Existe un individuo formateado" do
+			expect(@Ayrton.to_s).to eq("[Numero De Historia=#{@Ayrton.numero_historia}, Nombre=#{@Ayrton.nombre}, Apellido=#{@Ayrton.apellido}, Fecha De Nacimiento=#{@Ayrton.fecha_nacimiento}, Ocupacion=#{@Ayrton.ocupacion}, Fumador=#{@Ayrton.fumador}, Sexo=#{@Ayrton.sexo}]:")
+		end	
 	end
 end
