9efc6f7e135152568863a91a193c9a94a2f7fcae
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-11-20 22:06:28 +0000

    Creando el codigo para las caracteristicas del individuo

--- lib/gema/individuo.rb
+++ lib/gema/individuo.rb
@@ -1,2 +1,24 @@
+class Individuo
+	
+	attr_accessor :numero_historia, :nombre, :apellido, :fecha_nacimiento, :ocupacion, :fumador, :sexo
+	def initialize(numero_historia, nombre, apellido, fecha_nacimiento, ocupacion, fumador, sexo)
+		@numero_historia, @nombre, @apellido, @fecha_nacimiento, @ocupacion, @fumador, @sexo = numero_historia, nombre, apellido, fecha_nacimiento, ocupacion, fumador, sexo
+	end
 
+	def to_s
+		"[Numero De Historia=#{@numero_historia}, Nombre=#{@nombre}, Apellido=#{@apellido}, Fecha De Nacimiento=#{@fecha_nacimiento}, Ocupacion=#{@ocupacion}, Fumador=#{@fumador}, Sexo=#{@sexo}]:"
+	end
+end
+
+
+class Paciente < Individuo 
+
+
+#	attr_accessor :peso, :talla
+#	def initialize(numero_historia, nombre, apellido, fecha_nacimiento, ocupacion, fumador, sexo, peso, talla, cintura, cadera, bicipital, tricipital, subescapular, suprailiaco, brazo)
+#		super(numero_historia, nombre, apellido, fecha_nacimiento, ocupacion, fumador, sexo)
+#		@peso, @talla, @cintura, @cadera, @bicipital, @tricipital, @subescapular, @suprailiaco, @brazo = peso, talla, cintura, cadera, bicipital, tricipital, subescapular, suprailiaco, brazo	
+#	end
+
+end
 
