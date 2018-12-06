a76fbddaaac1e4703eb323eb276676273c94243d
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-12-06 12:25:22 +0000

    Creando el codigo para los alimentos

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,54 +1,59 @@
-class PruebasNutricion
-	
-	def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal)
-		@nombre, @grasas, @grasas_saturadas, @hidratos_carbono, @azucares, @proteinas, @sal = nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal
-	end
+module PruebasNutricion
 
-	def nombre_etiqueta
-		@nombre
-	end
+	class PruebasNutricion
 
-	def grasas
-		@grasas
-	end
+		include Comparable
 	
-	def grasas_saturadas
-		@grasas_saturadas
-	end
+		def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal)
+			@nombre, @grasas, @grasas_saturadas, @hidratos_carbono, @azucares, @proteinas, @sal = nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal
+		end
+
+		def nombre_etiqueta
+			@nombre
+		end
+
+		def grasas
+			@grasas
+		end
+	
+		def grasas_saturadas
+			@grasas_saturadas
+		end
 		
-	def hidratos_carbono
-		@hidratos_carbono
-	end
+		def hidratos_carbono
+			@hidratos_carbono
+		end
 
-	def azucares
-		@azucares
-	end
+		def azucares
+			@azucares
+		end
 
-	def proteinas
-		@proteinas
-	end
+		def proteinas
+			@proteinas
+		end
 
-	def sal
-		@sal
-	end
+		def sal
+			@sal
+		end
 
-	def kilojulios
-		(@grasas*37)+(@hidratos_carbono*17)+(@proteinas*17)+(@sal*25)
-	end
+		def kilojulios
+			(@grasas*37)+(@hidratos_carbono*17)+(@proteinas*17)+(@sal*25)
+		end
 	
-	def kilocalorias
-		(@grasas*9)+(@hidratos_carbono*4)+(@proteinas*4)+(@sal*6)
-	end
+		def kilocalorias
+			(@grasas*9)+(@hidratos_carbono*4)+(@proteinas*4)+(@sal*6)
+		end
 
-	def ingesta_recomendada
-		(@grasas*100)/70
-	end
+		def ingesta_recomendada
+			(@grasas*100)/70
+		end
 
-	def ingesta_recomendada_proteinas
-		(@proteinas*100)/50
-	end
+		def ingesta_recomendada_proteinas
+			(@proteinas*100)/50
+		end
 
-	def to_s
-		"[#{nombre_etiqueta}, Grasas=#{grasas}, GrasasSaturadas=#{grasas_saturadas}, HidratosCarbono=#{hidratos_carbono}, Azucares=#{azucares}, Proteinas=#{proteinas}, Sal=#{sal}]:"
-	end
-end
+		def to_s
+			"[#{nombre_etiqueta}, Grasas=#{grasas}, GrasasSaturadas=#{grasas_saturadas}, HidratosCarbono=#{hidratos_carbono}, Azucares=#{azucares}, Proteinas=#{proteinas}, Sal=#{sal}]:"
+		end
+	end	
+end	
