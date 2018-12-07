cc414f22cc2dd513ed5b868e9f387e5bad98550e
Ayrton Crespo <alu0100839739@ull.edu.es>
2018-12-07 09:22:35 +0000

    Poniendo comentarios

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,12 +1,24 @@
+
+#Modulo Pruebas de Nutricion que incluye las clases y sus métodos
+
 module PruebasNutricion
 
+	#Clase de las pruebas de la nutricion con atributos nombre, grasas, grasas saturadas, hidratos de carbono, azucares, proteinas y sal
+	
 	class PruebasNutricion
 
+
+		#Inclyendo el modulo comparable para comparar dos alimentos
+	
 		include Comparable
+	
+		#Método initialize para definir todos los atributos de la clase
 	
 		def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal)
 			@nombre, @grasas, @grasas_saturadas, @hidratos_carbono, @azucares, @proteinas, @sal = nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal
 		end
+
+		#Funciones para implementar los atributos
 
 		def nombre_etiqueta
 			@nombre
@@ -36,25 +48,37 @@ module PruebasNutricion
 			@sal
 		end
 
+		#Funcion para calcular los kilojulios de un alimento
+
 		def kilojulios
 			(@grasas*37)+(@hidratos_carbono*17)+(@proteinas*17)+(@sal*25)
 		end
+
+		#Funcion para calcular las kilocalorias de un alimento
 	
 		def kilocalorias
 			(@grasas*9)+(@hidratos_carbono*4)+(@proteinas*4)+(@sal*6)
 		end
 
+		#Funcion para calcular la ingesta recomandada de un alimento
+
 		def ingesta_recomendada
 			(@grasas*100)/70
 		end
+
+		#Funcion para calcular la ingesta recomendada de las proteinas de un alimento
 
 		def ingesta_recomendada_proteinas
 			(@proteinas*100)/50
 		end
 
+		#Método para mostrar el flujo de salida formateado
+
 		def to_s
 			"[#{nombre_etiqueta}, Grasas=#{grasas}, GrasasSaturadas=#{grasas_saturadas}, HidratosCarbono=#{hidratos_carbono}, Azucares=#{azucares}, Proteinas=#{proteinas}, Sal=#{sal}]:"
 		end
+
+		#Funcion para comparar los alimentos por las kilocalorias, <, >, ==, >=, <=
 		
 		def <=>(other)
 			kilocalorias <=> other.kilocalorias
